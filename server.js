3db8d41e826d7d841d1e56eac6dc268446fbb16e
David Stichter <david.stichter3414@gmail.com>
2016-02-21 17:52:21 -0500

    Requires, Listening, validation, empty views created

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 var express = require('express');
-var app = express();
 var expressHandlebars = require('express-handlebars');
 var bodyParser = require('body-parser');
 var mysql = require('mysql')
 var session = require('express-session')
 var Sequelize = require('sequelize')
 var sequelize = new Sequelize('lab', 'root');
+var bcrypt = require('bcryptjs');
 
 var Student = sequelize.define('Student', {
   firstname: Sequelize.STRING,
   lastname: Sequelize.STRING,
   email: Sequelize.STRING,
   classes: Sequelize.STRING,
+  username: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		unique: true
+	},
+	password: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			len: {
+				args: [5,32],
+				msg: "Your password must be between 5-32 characters"
+			},
+		}
+	}
+}, {
+	hooks: {
+		beforeCreate: function(input){
+			input.password = bcrypt.hashSync(input.password, 08301992);
+		}
+	}
 });
 var Teacher = sequelize.define('Teacher', {
   firstname: Sequelize.STRING,
@@ -19,6 +40,27 @@ var Teacher = sequelize.define('Teacher', {
   email: Sequelize.STRING,
   classes: Sequelize.STRING,
   ta: Sequelize.BOOLEAN
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [5,32],
+        msg: "Your password must be between 5-32 characters"
+      },
+    }
+  }
+}, {
+	hooks: {
+		beforeCreate: function(input){
+			input.password = bcrypt.hashSync(input.password, 08301992);
+		}
+	}
 });
 
 var PORT = process.env.NODE_ENV || 8000;
@@ -39,3 +81,9 @@ app.use(session({
   saveUninitialized: true,
   resave: false
 }));
+
+connection.sync().then(function() {
+  app.listen(port, function() {
+      console.log("Listening on:" + port)
+  });
+});
